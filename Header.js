2f872e03bee9cf5a59440de55fbdc04fbacafa68
Andrewkingz <andrewboytie@gmail.com>
2022-02-03 00:31:13 +0100

    Hero

---
+++ Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Header = () => {
+    return(
+        <div style={{ width: "100%", height: "100px", backgroundColor: "black", display: "flex", alignItems: "center", justifyContent: "space-around"}}>
+            <div style={{ width: "500px", display: "flex", justifyContent: "space-around"}}>
+                <a href='#' style={{color: "white", textDecoration: "none"}}>Home</a>
+                <a href='#' style={{color: "white", textDecoration: "none"}}>About</a>
+                <a href='#' style={{color: "white", textDecoration: "none"}}>Best Deals</a>
+                <a href='#' style={{color: "white", textDecoration: "none"}}>Tools</a>
+                <a href='#' style={{color: "white", textDecoration: "none"}}>Contact</a>
+
+
+
+
+
+            </div>
+
+           
+
+
+        </div>
+
+
+
+
+    );
+}
+export default Header
---
+++ Hero.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = () => {
+    return(
+        <div style={{ height: "500px", backgroundColor: "blue", color: "white", display: "flex", justifyContent: "center", alignItems: "center"}}>
+            <h2>The perfect lightweight theme for your next project.</h2>
+
+
+
+
+
+        </div>
+    )
+}
+export default Hero
